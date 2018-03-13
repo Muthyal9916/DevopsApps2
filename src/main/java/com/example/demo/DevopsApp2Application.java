800dc0a4c92983b8ebd649f65ef2880b9ce1ef96
muthu <muthuatp.k@gmail.com>
2018-03-13 15:08:22 +0530

    Commit the Changes

---
+++ src/main/java/com/example/demo/DevopsApp2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsApp2Application.class, args);
+	}
+}
